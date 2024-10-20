67d51bb14bcabc48517a33f963f34bfe292e4146
Pooja Sharma <sharmapooja4917@gmail.com>
2024-10-20 10:07:49 +0530

    Gird basic example done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Example1 from "./components/Example1";
+
+function App() {
+  return (
+    <div>
+      <Example1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Example1.jsx
@@ -0,0 +1,43 @@
+import { AgGridReact } from "ag-grid-react"
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-quartz.css"; 
+import { useState } from "react";
+
+
+
+const Example1 = () => {
+
+    // Row Data: The data to be displayed.
+    const [rowData ] = useState([
+        { make: 'Tesla', model: 'Model Y', price: 64950, electric: true },
+        { make: 'Ford', model: 'F-Series', price: 33850, electric: false },
+        { make: 'Toyota', model: 'Corolla', price: 29600, electric: false },
+        { make: 'Mercedes', model: 'EQA', price: 48890, electric: true },
+        { make: 'Fiat', model: '500', price: 15774, electric: false },
+        { make: 'Nissan', model: 'Juke', price: 20675, electric: false },
+    ])
+
+    // Column Definitions: Defines the columns to be displayed.
+    const [colDefs] = useState([
+        { field: 'make' },
+        { field: 'model' },
+        { field: 'price' },
+        { field: 'electric' },
+    ])
+
+    const defaultColDef = {
+        flex : 1
+    }
+
+  return (
+    <div className="ag-theme-quartz" style={{height: 500}}>
+       <AgGridReact 
+       rowData={rowData} 
+       columnDefs={colDefs}
+       defaultColDef={defaultColDef}
+       />
+    </div>
+  )
+}
+
+export default Example1
