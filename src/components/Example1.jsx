8a86e5c76ccf4e98c1cb7453affebc70d5680e66
Pooja Sharma <sharmapooja4917@gmail.com>
2024-10-20 11:03:56 +0530

    added filter , sorting , pagination and editable properties

--- src/components/Example1.jsx
+++ src/components/Example1.jsx
@@ -1,42 +1,83 @@
-import { AgGridReact } from "ag-grid-react"
+import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
-import "ag-grid-community/styles/ag-theme-quartz.css"; 
+import "ag-grid-community/styles/ag-theme-quartz.css";
 import { useState } from "react";
 
 
 const Example1 = () => {
+  // Row Data: The data to be displayed.
+  const [rowData] = useState([
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+    { make: "Mercedes", model: "EQA", price: 48890, electric: true },
+    { make: "Fiat", model: "500", price: 15774, electric: false },
+    { make: "Nissan", model: "Juke", price: 20675, electric: false },
+  ]);
 
-    // Row Data: The data to be displayed.
-    const [rowData ] = useState([
-        { make: 'Tesla', model: 'Model Y', price: 64950, electric: true },
-        { make: 'Ford', model: 'F-Series', price: 33850, electric: false },
-        { make: 'Toyota', model: 'Corolla', price: 29600, electric: false },
-        { make: 'Mercedes', model: 'EQA', price: 48890, electric: true },
-        { make: 'Fiat', model: '500', price: 15774, electric: false },
-        { make: 'Nissan', model: 'Juke', price: 20675, electric: false },
-    ])
+  // add buttons to cells with Cell Component
+  const CustomButtonComponent = () => {
+    return <button onClick={() => alert("clicked")}>Push me</button>;
+  };
+  // Column Definitions: Defines the columns to be displayed.
+  const [colDefs] = useState([
+    {
+      headerName: "Company",
+      field: "make",
+      valueGetter: (p) => p.data.make + " " + p.data.model,
+      flex: 2,
+    }, // if header name is not present , the title is taken from the field
+    {
+      field: "model",
+      filter: true,
+      floatingFilter: true,
+      editable: true,
+      cellEditor: "agSelectCellEditor",
+      cellEditorParams: {
+        values: ["Juke", "500", "EQA", "Model Y"],
+      },
+    },
+    {
+      field: "price",
+      valueFormatter: (p) => "£" + p.value.toLocaleString(),
+      filter: true,
+    },
+    { field: "electric", editable: true },
+    { field: "button", cellRenderer: CustomButtonComponent },
+  ]);
 
-    // Column Definitions: Defines the columns to be displayed.
-    const [colDefs] = useState([
-        { headerName : 'Company', field: 'make' , valueGetter: p => p.data.make + ' ' + p.data.model }, // if header name is not present , the title is taken from the field
-        { field: 'model'  },
-        { field: 'price', valueFormatter: p => '£' + p.value.toLocaleString()  },
-        { field: 'electric' },
-    ])
+  const defaultColDef = {
+    flex: 1,
+    sortable: false
+    // filter: true,
+    // floatingFilter: true
+  };
 
-    const defaultColDef = {
-        flex : 1
-    }
+  // row selection
+  const rowSelection = {
+    // mode : 'singleRow'
+    mode: "multiRow",
+    headerCheckbox: false,
+  };
+
+  const pagination = true
+  const paginationPageSize = 2;
+  const paginationPageSizeSelector = [2 , 4,6];
+
 
   return (
-    <div className="ag-theme-quartz" style={{height: 500}}>
-       <AgGridReact 
-       rowData={rowData} 
-       columnDefs={colDefs}
-       defaultColDef={defaultColDef}
-       />
+    <div className="ag-theme-quartz" style={{ height: 500 }}>
+      <AgGridReact
+        rowData={rowData}
+        columnDefs={colDefs}
+        defaultColDef={defaultColDef}
+        rowSelection={rowSelection}
+        pagination = {pagination}
+        paginationPageSize={paginationPageSize}
+        paginationPageSizeSelector={paginationPageSizeSelector}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Example1
+export default Example1;
